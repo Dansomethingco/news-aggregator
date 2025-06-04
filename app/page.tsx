741e474bcdfcb0bed7c46edc96f2c2210993c9c8
Dansomethingco <dan.somethingco@gmail.com>
2025-06-04 16:07:36 +0100

    Remove Today's News title section

--- app/page.tsx
+++ app/page.tsx
@@ -91,12 +91,6 @@ export default function Home() {
         </div>
       </header>
 
-      <div className="bg-red-500 text-white py-4">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h1 className="text-2xl font-bold">Today's News</h1>
-        </div>
-      </div>
-
       <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <div className="flex flex-col">
           {isLoading && (
